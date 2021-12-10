f405525f1545a4efac01abe508b90751098b0580
abel-parada <abel.parada.millan@gmail.com>
2021-12-10 15:04:50 +0200

    readme.md done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
+import React from 'react';
+
 const Footer = () => {
     return (
-        <footer>
-            <p>
-                <small>Abel Parada - All rights reserved</small>
-            </p>
-        </footer>
+        <div className="App">
+            <footer className="footer">
+                <p>Abel Parada. &copy; 2021. Visit my <strong><a href="https://github.com/abel-parada">Github Account</a></strong></p>
+            </footer>
+        </div>
     );
-}; 
+};
 
 export default Footer;
